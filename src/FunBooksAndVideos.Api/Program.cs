6c27381816398f5ec614e8b2741e0c56459d1cb6
Jmuddiman666 <12989976+Jmuddiman666@users.noreply.github.com>
2022-07-22 01:25:41 +0100

    Register DI

--- src/FunBooksAndVideos.Api/Program.cs
+++ src/FunBooksAndVideos.Api/Program.cs
@@ -1,3 +1,6 @@
+using FunBooksAndVideos.Api.Interfaces;
+using FunBooksAndVideos.Api.Processors;
+using FunBooksAndVideos.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IMembershipService, MembershipService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderProcessor, PurchaseOrderProcessor>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
